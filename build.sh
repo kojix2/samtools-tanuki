2f3db3ef48ddadd4cc4715d95e302b48c8004d1a
kojix2 <2xijok@gmail.com>
2023-12-09 21:16:05 +0900

    Update

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
+#!/bin/sh
+set -e
+set -u
+
+git submodule update --init --recursive
+
 CONFIG=./myconfig.rb rake -f mruby/Rakefile
 cd samtools
 autoreconf -i
 ./configure
 make
+samtools/samtools
